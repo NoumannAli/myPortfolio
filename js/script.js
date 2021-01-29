c9a85740f1d3e24f3cd9d2a33e878eae723fdf5a
Nouman <alinouman003@gmail.com>
2021-01-28 22:43:01 -0500

    modify index.html, style.css,script.js?

--- js/script.js
+++ js/script.js
@@ -35,16 +35,16 @@ for (let i = 0; i < totalFilterBtn; i++) {
 
 
 
-var readme_btn = document.querySelectorAll('.read-me-btn');
-var readme_txt = document.querySelectorAll('.read-me');
+let readme_btn = document.querySelectorAll('.read-me-btn');
+let readme_txt = document.querySelectorAll('.read-me');
 
 
-for (var k = 0; k < readme_txt.length; k++) {
+for (let k = 0; k < readme_txt.length; k++) {
 
   readme_txt[k].style.display = "none";
 }
 
-for (var i = 0; i < readme_btn.length; i++) {
+for (let i = 0; i < readme_btn.length; i++) {
 
   readme_btn[i].addEventListener("click", function() {
 
@@ -71,14 +71,9 @@ for (var i = 0; i < readme_btn.length; i++) {
   });
 }
 
-
-
-
-
-
-var navBar = document.querySelector("#nav-bar");
-var toggle = document.querySelector(".toggler");
-var toggler = document.querySelector(".toggler .fa");
+let navBar = document.querySelector("#nav-bar");
+let toggle = document.querySelector(".toggler");
+let toggler = document.querySelector(".toggler .fa");
 
 navBar.style.left = "-270px";
 toggle.addEventListener("click", function() {
@@ -99,12 +94,12 @@ toggle.addEventListener("click", function() {
 
 
 
-var nav_items = document.querySelectorAll("ul li");
+let nav_items = document.querySelectorAll("ul li");
 
-for (var i = 0; i < nav_items.length; i++) {
+for (let i = 0; i < nav_items.length; i++) {
   const a = nav_items[i].querySelector("a");
-  a.addEventListener("click", function() {
-    for (let j = 0; j < nav_items.length; j++) {
+  a.addEventListener("click", function(){
+    for (let j = 0; j < nav_items.length; j++); {
       navBar.style.left = "-270px";
 
       toggler.style.left = "2%";
@@ -112,98 +107,14 @@ for (var i = 0; i < nav_items.length; i++) {
 
     }
 
-  })
+  });
 }
-
-
-
 AOS.init({
   easing: 'ease',
   duration: 1800
 });
 
-
-// jquery
-// var read_me = $(".read-me");
-
-
-// $(".read-me-btn").click(function(event){
-
-//    console.log(this);
-//   $(this.nextElementSibling).slideToggle("slow");
-//      if (this.nextElementSibling.style.display === "none"){
-//           this.nextElementSibling.style.display = "block";
-//           this.innerHTML = "Read me";
-//      }else if (this.nextElementSibling.style.display === "block"){
-//                this.nextElementSibling.style.display = "none";
-//               this.innerHTML = "Close me";
-//      }
-//     // read_me.stopPropagation();
-//     // read_me.stopImmediatePropagation();
-
-
-
-// })
-
-
-//   const nav = document.querySelector(".nav"),
-//   navList = nav.querySelectorAll("li"),
-//   totalNavList = navList.length;
-//   allSection = document.querySelectorAll(".section"),
-//   totalSection = allSection.length;
-
-
-//   for(let i = 0 ; i <totalNavList ; i++){
-//   	const a=navList[i].querySelector("a");
-//   	a.addEventListener("click",function(){
-//   		for(let j = 0 ; j < totalNavList; j++){
-//   			navList[j].querySelector("a").classList.remove("active");
-//   		}
-//   		this.classList.add("active");
-//   		showSection(this);
-
-//   	})
-//   }
-
-
-//          function showSection(element){
-//          	for(let i = 0 ; i<totalSection;i++){
-//          		allSection[i].classList.remove("active");
-//          	}
-
-//          	const target = element.getAttribute("href").split("#")[1];
-//          	document.querySelector("#"+target).classList.add("active")
-//          }
-
-
-//      const navTogglerBtn = document.querySelector(".nav-toggler"),
-//      aside=document.querySelector(".aside");
-
-//      navTogglerBtn.addEventListener("click",()=>{
-//      	asideSectionTogglerBtn();
-//      })
-
-// function asideSectionTogglerBtn(){
-// 	aside.classList.toggle("open");
-// 	navTogglerBtn.classList.toggle("open");
-
-// }
-
-
-// $(document).ready(function () {
-
-//     $("#sidebar").mCustomScrollbar({
-//          theme: "minimal"
-//     });
-
-//     $('#sidebarCollapse').on('click', function () {
-//         // open or close navbar
-//         $('#sidebar').toggleClass('active');
-//         // close dropdowns
-//         $('.collapse.in').toggleClass('in');
-//         // and also adjust aria-expanded attributes we use for the open/closed arrows
-//         // in our CSS
-//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-//     });
-
-// });
+ let scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 300,
+      speedAsDuration: true
+    });
